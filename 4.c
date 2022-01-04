afc624f5255dd1a61511288489598e271ebad50c
Ilzira1010 <65736412+Ilzira1010@users.noreply.github.com>
2022-01-04 12:51:05 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -4,26 +4,32 @@
 int main(int argc, char *argv[]){
 	FILE *file;
 	file = fopen(argv[1], "r");
-	int N =strtol(argv[2], NULL, 10);
+	int N = strtol(argv[2], NULL, 10);
 	if (file == NULL) {
-		printf("Can't open the file_first");
+		printf("Can't open the files");
 		return -1;
 	}
 	
-	char key = ' ';
-	char count =0;
-	int i=0;
-	while ((count = fgetc(file)) != EOF) {
-		if ((i == N) && (N !=0))  {
-		scanf("%c", &key);
-		i = 0;
+	int count = 1;
+	int a;
+	while ((a = fgetc(file)) != EOF) {
+		if ((count == N) && (N !=0) && (a == '\n')) {
+			count = 1;
+			if(fgetc(stdin)) {
+				printf("\n");
+			}
+			continue;
 		}
-		printf("%c", count);
-		i++;
+		if (count <= N) {
+			printf("%c", a);
+		}
+		if ( a == '\n'){
+			count++;
+		}
 	}
 
 	if (fclose(file) == EOF) {
-		printf("Can't close the file_first");
+		printf("Can't close the file");
 		return -1;
 	} 
 	return 0;
